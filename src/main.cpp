69802eba9fce349be2d2359a4ff6efdf381ba29f
tuvosyl <valentin.soltys@gmail.com>
2026-06-30 14:31:01 +0200

    switch from qt to FTXUI

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,36 @@
-#include <QApplication>
-#include <QPushButton>
+#include <ftxui/component/component.hpp>
+#include <ftxui/component/screen_interactive.hpp>
+#include <ftxui/dom/elements.hpp>
 #include <spdlog/spdlog.h>
 
-int main(int argc, char *argv[])
+int main()
 {
-    QApplication app(argc, argv);
     spdlog::info("gomoku started");
 
-    QPushButton button("Click me");
-    button.resize(200, 60);
-    button.show();
+    auto screen = ftxui::ScreenInteractive::Fullscreen();
+    ftxui::ButtonOption opt;
+    opt.transform = [](const ftxui::EntryState& s) {
+        auto label = ftxui::text(s.label) | ftxui::center;
+        if (s.focused)
+            label = label | ftxui::bold;
+        return label | ftxui::borderRounded | ftxui::bgcolor(ftxui::Color::Default);
+    };
+    auto button = ftxui::Button("Quitter", screen.ExitLoopClosure(), opt);
 
-    QObject::connect(&button, &QPushButton::clicked, &app, &QApplication::quit);
+    auto renderer = ftxui::Renderer(button, [&] {
+        return ftxui::hbox({
+                   ftxui::filler(),
+                   ftxui::vbox({
+                       ftxui::filler(),
+                       button->Render(),
+                       ftxui::filler(),
+                   }),
+                   ftxui::filler(),
+               }) |
+               ftxui::bgcolor(ftxui::Color::Default);
+    });
 
-    return app.exec();
+    screen.Loop(renderer);
+
+    return 0;
 }
