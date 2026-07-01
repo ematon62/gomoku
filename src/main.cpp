32aa8500701c7b019558b98a0ff4d9bc57664761
ematon <emanuel.maton1@gmail.com>
2026-07-01 16:50:15 +0200

    fixed main

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,34 @@
 #include "gomoku.hpp"
 
 int main() {
-    auto logger = get_logger();
-    logger.info("Logger spawned.");
+  auto logger = get_logger();
+  logger.info("Logger spawned.");
 
-    auto screen = ftxui::ScreenInteractive::Fullscreen();
-    ftxui::ButtonOption opt;
-    opt.transform = [](const ftxui::EntryState &s) {
-        auto label = ftxui::text(s.label) | ftxui::center;
-        if (s.focused)
-            label = label | ftxui::bold;
-        }
-        return label | ftxui::borderRounded | ftxui::bgcolor(ftxui::Color::Default);
-    };
-    auto button = ftxui::Button("Quitter", screen.ExitLoopClosure(), opt);
+  auto screen = ftxui::ScreenInteractive::Fullscreen();
+  ftxui::ButtonOption opt;
+  opt.transform = [](const ftxui::EntryState &start) {
+    auto label = ftxui::text(start.label) | ftxui::center;
+    if (start.focused) {
+      label = label | ftxui::bold;
+    }
+    return label | ftxui::borderRounded | ftxui::bgcolor(ftxui::Color::Default);
+  };
+  auto button = ftxui::Button("Quitter", screen.ExitLoopClosure(), opt);
 
-    auto renderer = ftxui::Renderer(button, [&] {
-        return ftxui::hbox({
+  auto renderer = ftxui::Renderer(button, [&] {
+    return ftxui::hbox({
+               ftxui::filler(),
+               ftxui::vbox({
                    ftxui::filler(),
-                   ftxui::vbox({
-                       ftxui::filler(),
-                       button->Render(),
-                       ftxui::filler(),
-                   }),
+                   button->Render(),
                    ftxui::filler(),
-               }) |
-               ftxui::bgcolor(ftxui::Color::Default);
-    });
+               }),
+               ftxui::filler(),
+           }) |
+           ftxui::bgcolor(ftxui::Color::Default);
+  });
 
-    screen.Loop(renderer);
+  screen.Loop(renderer);
 
-    return 0;
+  return 0;
 }
